8960852d0957cb8208a3c48140e57efeb1976b29
Shubhankar Sengupta <dev3@paralleldigital.io>
2026-06-01 06:18:52 +0530

    Implemented Api gateway

---
+++ TMP-API-Gateway/src/main/java/com/tmp/api_gateway/TmpApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.tmp.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmpApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmpApiGatewayApplication.class, args);
+	}
+
+}
